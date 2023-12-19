b0303f1b811f2ad30fd816dfacda9b468f2db8a7
alifeee <alifeeerenn@hotmail.com>
2023-12-19 16:57:30 +0000

    switch from mustache to handlebars and add date helper so I can use helpers

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,14 @@
 module.exports = function (eleventyConfig) {
   // pass everything in public to root
   eleventyConfig.addPassthroughCopy({ public: "/" });
+
+  // add date handler
+  eleventyConfig.addFilter("dateDisplay", (dateObj) => {
+    let date = new Date(dateObj);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  });
 };
